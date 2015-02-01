79736bef0c2c42e6bf4a5a67820e11f6c23fff9f
Aurelien Chantioux <brad.inside@gmail.com>
2015-02-01 22:03:06 +0100

    starting view with 2 directions and load the trace

--- js/map_loader.js
+++ js/map_loader.js
@@ -1,61 +1,94 @@
 
-//google.maps.event.addDomListener(window, 'load', startConverter );
-function initialize() {
-        var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', startConverter );
 var bradymap = null;
 
 function startConverter(){
-	alert('hello');
 	bradymap = new bradyMapLoader('map-canvas');
 	bradymap.initialize();
+	bradymap.setAutocomplete('start_pt');
+	bradymap.setAutocomplete('end_pt');
 }
 
 
 
 function bradyMapLoader(mapContainerId){
 	
+	// id of the container where ggmap will inject his code
 	this.mapContainerId = mapContainerId;
+	//render options
 	this.rendererOptions = {
   		draggable: true
 	};
+
+	// center of the map 
 	this.startTown = new google.maps.LatLng(45.75, 4.85);
+	// zoom level
 	this.zoom = 7;
+	// computed options
 	this.mapOptions = {
-		zoom:7,
+		zoom:this.zoom,
 		center:this.startTown
 	};
+	// will contain the ggmap object
 	this.map = null;
+	// directionDisplay service
 	this.directionsDisplay = new google.maps.DirectionsRenderer(this.rendererOptions);
+	// direction service
 	this.directionsService = new google.maps.DirectionsService();
+	// store autocompletes services
+	this.autocompletes = [];
 
 
+	// initialize the converter
 	this.initialize = function (){
+		// creating the map
 		this.map = new google.maps.Map(
 			document.getElementById(this.mapContainerId),
 			this.mapOptions);
+		// linkin the direction service with the map
 		this.directionsDisplay.setMap(this.map);
+		// listen to draggable events
 		google.maps.event.addListener(this.directionsDisplay, 'directions_changed', function() {
 				this.onDirectionsChanged();
   		});
 	}
 
-	this.loadMap = function(url)
-	{
-		var _iframe = '<iframe id="temp_iframe" src="'+url+'"></iframe>';
-		$('#map_iframe_container').append(_iframe);
-		$('#map_iframe_container').load(function(){
-			var iframe = $('#filecontainer').contents();
-		});
+	this.setAutocomplete = function (inputId){
+		// load the autocomplete service on a field
+		var autocomplete = new google.maps.places.Autocomplete(document.getElementById(inputId) );
+		// store it in a tab var.
+		this.autocompletes[''+inputId] =  autocomplete;
+		google.maps.event.addListener(autocomplete, 'place_changed', function(inputId,autocomplete) {
+		    bradymap.onAutoCompleteChanged();
+		  });
 	}
 
+
 	this.onDirectionsChanged = function(){
-		console.log(directionsDisplay.getDirections());
+		console.log('directionChanged');
+		//console.log(this.directionsDisplay.getDirections());
+	}
+
+	this.onAutoCompleteChanged = function (inputId,autocomplete){
+		
+		if( $('#start_pt').val() != '' && $('#end_pt').val() != ''){
+			this.drawingRoute([$('#start_pt').val(), $('#end_pt').val()]);
+		}
+	}
+
+	this.drawingRoute = function(points){
+		var start = points[0];
+		var end = points[points.length-1];
+		  var request = {
+		      origin:start,
+		      destination:end,
+		      travelMode: google.maps.TravelMode.DRIVING
+		  };
+		  this.directionsService.route(request, function(response, status) {
+		    console.log('ok');
+		    if (status == google.maps.DirectionsStatus.OK) {
+		      bradymap.directionsDisplay.setDirections(response);
+		    }
+		  });
 	}
 }
